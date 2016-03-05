d64595cbc6f9891134f913762102747ca62b9775
BrandenSandahl <branden157@gmail.com>
2016-03-05 15:22:45 -0500

    new .jar

--- src/com/teamSmash/Main.java
+++ src/com/teamSmash/Main.java
@@ -50,7 +50,7 @@ public class Main {
 
         stmt.setString(1, name);
         stmt.setString(2, location);
-        stmt.setString(3, (time));  //here I am needing to convert a LocalTime object into a Time object with the DB will accept more freely. I think.
+        stmt.setString(3, time);  //here I am needing to convert a LocalTime object into a Time object with the DB will accept more freely. I think.
         stmt.setDate(4, (parsedDate));  //same here but for Date.
         stmt.setString(5, image);
         stmt.setString(6, description);
@@ -250,6 +250,7 @@ public class Main {
         String description = results.getString(7);
         int accountId = results.getInt(8);
         Event event = new Event(id, name, location, time, date.toLocalDate(), image, description, accountId);
+      //  public Event(int id, String name, String location, String time, LocalDate date, String image, String description, int eventOwner) {
         return event;
     }
 
@@ -334,14 +335,14 @@ public class Main {
 
                     String name = request.queryParams("eventName");
                     String location = request.queryParams("eventLocation");
-                    LocalDate date = null;
-                    LocalTime time = null;
+                    String date = null;
+                    String time = null;
                     if(!request.queryParams("time").equals("")){
-                        time = LocalTime.parse(request.queryParams("time"));
+                        time = request.queryParams("time");
                     }
 
                     if(!request.queryParams("date").equals("")){
-                        date = LocalDate.parse(request.queryParams("date"));
+                        date = request.queryParams("date");
                     }
 //                    LocalTime time = LocalTime.parse(request.queryParams("time"));
 //                    LocalDate date = LocalDate.parse(request.queryParams("date"));
